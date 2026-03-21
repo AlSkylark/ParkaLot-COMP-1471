92b5b01f5763baf7bbd078fab38e3c8df396a136
Alex Castro <alxc119@gmail.com>
2026-03-21 00:54:26 +0000

    feat(PAR-19): Adds a set of basic security utilities and basic auth (email only)

---
+++ backend/src/main/java/com/parkalot/api/customer/CustomerController.java
@@ -0,0 +1,55 @@
+package com.parkalot.api.customer;
+
+import com.parkalot.infrastructure.security.CustomerAuthService;
+import com.parkalot.infrastructure.security.JwtUtil;
+import java.util.Map;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CustomerController {
+
+  private final CustomerAuthService auth;
+  private final JwtUtil jwt;
+  private final CustomerService service;
+
+  public CustomerController(
+    CustomerAuthService auth,
+    JwtUtil jwt,
+    CustomerService service
+  ) {
+    this.auth = auth;
+    this.jwt = jwt;
+    this.service = service;
+  }
+
+  @GetMapping("/profile")
+  public ResponseEntity<?> getMethodName() {
+    var auth = SecurityContextHolder.getContext().getAuthentication();
+    if (auth == null || !auth.isAuthenticated()) {
+      return ResponseEntity.status(401).build();
+    }
+
+    var customer = service.GetCustomer(auth.getName());
+    return ResponseEntity.ok(customer);
+  }
+
+  @PostMapping("/login")
+  public ResponseEntity<?> login(@RequestBody String email) {
+    try {
+      auth.loadUserByUsername(email);
+    } catch (UsernameNotFoundException e) {
+      return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(
+        Map.of("error", e.getMessage())
+      );
+    }
+
+    return ResponseEntity.ok(Map.of("token", jwt.generate(email)));
+  }
+}
